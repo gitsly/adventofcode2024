d4b1c22542866d45b54b78d7dbcb262733366a54
Martin Collberg <martin.collberg@gmail.se>
2024-12-01 13:41:45 +0100

    parse input

--- src/adventofcode2024/day1.clj
+++ src/adventofcode2024/day1.clj
@@ -2,10 +2,22 @@
   (:require [clojure.string :as str]
             [adventofcode2024.utils :as u]))
 
-(def input-pt1 ["1abc2"
-                "pqr3stu8vwx"
-                "a1b2c3d4e5f"
-                "treb7uchet"])
+(def input (u/get-lines "resources/day1/sample"))
 
-;;(def input-mine (u/get-lines "resources/day1.txt"))
+(let [
+      parse-line (fn[line]
+                   (->> line
+                        (re-seq #"(\d+)\s*")
+                        (map last)
+                        (map Integer/parseInt)))
+
+      parse-input (fn[lines]
+                    (map parse-line lines))
+
+      input (parse-input input)
+      a (map first input)
+      b (map second input)]
+
+  {:a a
+   :b b})
 
