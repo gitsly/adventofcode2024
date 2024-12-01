a2649ab052a5a2130b3c1288c29456ff34eb826d
Martin Collberg <martin.collberg@gmail.se>
2024-12-01 14:05:02 +0100

    solve part 1 and 2

--- src/adventofcode2024/day1.clj
+++ src/adventofcode2024/day1.clj
@@ -2,7 +2,7 @@
   (:require [clojure.string :as str]
             [adventofcode2024.utils :as u]))
 
-(def input (u/get-lines "resources/day1/sample"))
+(def input (u/get-lines "resources/day1/input"))
 
 (let [
       parse-line (fn[line]
@@ -10,14 +10,29 @@
                         (re-seq #"(\d+)\s*")
                         (map last)
                         (map Integer/parseInt)))
-
       parse-input (fn[lines]
                     (map parse-line lines))
 
       input (parse-input input)
-      a (map first input)
-      b (map second input)]
+      a (map first input) ; first liest
+      b (map second input) ; second location list
+      get-pair-info (fn[pair] (let [[a b] pair]
+                                {:a a
+                                 :b b
+                                 :distance (abs (- a b) )}))
 
-  {:a a
-   :b b})
+      similarity-score (fn[src
+                           b]
+                         (* src (count (filter #(= % src) b))))]
+
+  (println 
+   {:part1 (->> (map vector (sort a) (sort b))
+                (map get-pair-info)
+                (map :distance)
+                (reduce +))
+    :part2 
+    (reduce + 
+            (map #(similarity-score % b) a))}))
+
+
 
