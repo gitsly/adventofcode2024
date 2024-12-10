09944db96c295b08e4d93d0e844b61b000a9ae08
Martin Collberg <martin.collberg@gmail.se>
2024-12-10 21:30:46 +0100

    getting closer

--- src/adventofcode2024/day3.clj
+++ src/adventofcode2024/day3.clj
@@ -4,11 +4,13 @@
 
 (def input "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))")
 
-(re-seq #"mul\(\d{0,3}.*\)" input)
+(re-seq #"mul\(\d{1,3},\d{1,3}\)" input)
+;; 161 (2*4 + 5*5 + 11*8 + 8*5).
 
-(->> ["123", "1231" "aba"]
-     (map #(re-matches #"\d{0,3}" %))
-     )
+
+(->> ["123", "1231" "aba", "1"]
+     (map #(re-matches #"\d{1,3}" %))
+     (filter #(not (nil? %))))
 
 
 
