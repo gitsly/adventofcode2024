4f74cf7dbf1a63c9080a99772bf84ad8d79f9e99
Martin Collberg <martin.collberg@gmail.se>
2024-12-11 20:33:13 +0100

    doing the parse line

--- src/adventofcode2024/day3.clj
+++ src/adventofcode2024/day3.clj
@@ -3,9 +3,15 @@
             [adventofcode2024.utils :as u]))
 
 (def input "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))")
-
-(re-seq #"mul\(\d{1,3},\d{1,3}\)" input)
 ;; 161 (2*4 + 5*5 + 11*8 + 8*5).
+
+(let [muls (re-seq #"mul\((\d{1,3}),(\d{1,3})\)" input)
+
+      parse-mul (fn[mul] 
+                  (let [[_ & factors] mul] (map Integer/parseInt factors)))
+      ]
+  (parse-mul
+   (first muls)))
 
 
 (->> ["123", "1231" "aba", "1"]
