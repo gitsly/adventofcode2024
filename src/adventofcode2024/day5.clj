925b335315ba54efe845afb5d3f747b9202e2575
Martin Collberg <martin.collberg@gmail.se>
2024-12-25 12:11:57 +0100

    basic parsing setup

--- src/adventofcode2024/day5.clj
+++ src/adventofcode2024/day5.clj
@@ -2,3 +2,12 @@
   (:require [clojure.string :as str]
             [clojure.spec.alpha :as s]
             [adventofcode2024.utils :as u]))
+
+
+(let [lines (u/get-lines "resources/day5/sample")
+      input (let [coll lines
+                  pred (fn [item] (not (empty? item)))]
+              [(take-while pred coll) (drop 1 (drop-while pred coll))])] ; vector of ['page ordering rules', 'page numbers of each update']
+
+
+  )
