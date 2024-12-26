cd85f055651146fc58de0fb925143a17fb688eb9
Martin Collberg <martin.collberg@gmail.se>
2024-12-26 13:20:04 +0100

    wip

--- src/adventofcode2024/day5.clj
+++ src/adventofcode2024/day5.clj
@@ -12,8 +12,8 @@
       parse-page-order (fn[line]
                          "Returns a vector page number rule [before, after]"
                          (let [[_ before after] (first(re-seq #"(\d+)\|(\d+)" line))]
-                           [(Integer/parseInt before)
-                            (Integer/parseInt after)]))
+                           {(Integer/parseInt before)
+                            (Integer/parseInt after)}))
 
       parse-update (fn [line]
                      (vec (map Integer/parseInt
@@ -24,11 +24,32 @@
 
       upd (map parse-update upd)
 
-      check-update (fn [upd rules]
-                     nil)
+
+      before (fn [curr
+                  rules]
+               "Get sequence of pages that must be printed before 'curr'"
+               (filter #(not (nil? %)) 
+                       (map 
+                        #(get % curr) rules)))
+
+      heppas (fn [col  ; previously checked
+                  rules]
+               (let [curr (last col) ; current value to check
+                     check (fn[i col]
+                             (println col ":" i)
+                             (every? #(> % i) col))
+                     before (before curr rules)]
+
+                 (every? #(check % (drop-last col)) before)
+
+                 ))
 
       ]
 
-  [rules
-   upd]
+  ;; note: potential tree structure since multiple
+  ;; keys are identical (before) 
+
+  (heppas [74 47] rules) ; true
+  (heppas [74 47 61] rules) ; true
+
   )
