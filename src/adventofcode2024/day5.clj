d2fc41e5e8bb7ea2bf89c7c5c65ca1bca19cc5b8
Martin Collberg <martin.collberg@gmail.se>
2024-12-26 17:02:02 +0100

    after check

--- src/adventofcode2024/day5.clj
+++ src/adventofcode2024/day5.clj
@@ -1,7 +1,8 @@
 (ns adventofcode2024.day5
   (:require [clojure.string :as str]
             [clojure.spec.alpha :as s]
-            [adventofcode2024.utils :as u]))
+            [adventofcode2024.utils :as u]
+            [clojure.set :as set]))
 
 (let [lines (u/get-lines "resources/day5/sample")
       [rules upd] (let [coll lines
@@ -12,8 +13,8 @@
       parse-page-order (fn[line]
                          "Returns a vector page number rule [before, after]"
                          (let [[_ before after] (first(re-seq #"(\d+)\|(\d+)" line))]
-                           {(Integer/parseInt before)
-                            (Integer/parseInt after)}))
+                           [(Integer/parseInt before)
+                            (Integer/parseInt after)]))
 
       parse-update (fn [line]
                      (vec (map Integer/parseInt
@@ -32,17 +33,43 @@
                        (map 
                         #(get % curr) rules)))
       
-      check (fn [curr
-                 rules]
-              "Returns true if all rules for 'curr' is met"
-              (let [before (before curr rules)]
-                (every? #(< % curr) before)))
+      check-after (fn [curr
+                       rules]
+                    (let [before (before curr rules)]
+                      (every? #(< % curr) before)))
+
+      sample (first upd)
+
+      get-after-rules (fn [item
+                           rules]
+                        (filter #(= item (first %)) rules))
+
+      into-hash-map     (fn [rules]
+                          (map #(hash-map
+                                 (first %) (second %)) rules))
+      into-hash-map-rev (fn [rules]
+                          (map #(hash-map
+                                 (second %) (first %)) rules))
 
       ]
 
   ;; note: potential tree structure since multiple
   ;; keys are identical (before) 
 
-  (check 75 rules) ; true
+  ;; 75 is correctly first because there are rules that put each other page after it: 75|47, 75|61, 75|53, and 75|29. 
+  (let [curr 75
+        before []
+        after [47,61,53,29]
 
-  )
+        after-rules (get-after-rules curr rules)
+
+        after-nums (set/intersection 
+                    (set after)
+                    (set (map second after-rules)))
+
+        after-check (every? #(< % curr) after-nums)
+        ]
+
+    after-check
+
+    ))
