5974edd42ae4bd2ea2b08d0f3e39143c3b38a532
Martin Collberg <martin.collberg@gmail.se>
2024-12-26 11:00:46 +0100

    Parsing into int arrays complete

--- src/adventofcode2024/day5.clj
+++ src/adventofcode2024/day5.clj
@@ -3,11 +3,32 @@
             [clojure.spec.alpha :as s]
             [adventofcode2024.utils :as u]))
 
-
 (let [lines (u/get-lines "resources/day5/sample")
-      input (let [coll lines
-                  pred (fn [item] (not (empty? item)))]
-              [(take-while pred coll) (drop 1 (drop-while pred coll))])] ; vector of ['page ordering rules', 'page numbers of each update']
+      [rules upd] (let [coll lines
+                        pred (fn [item] (not (empty? item)))]
+                    [(take-while pred coll)
+                     (drop 1 (drop-while pred coll))])
 
+      parse-page-order (fn[line]
+                         "Returns a vector page number rule [before, after]"
+                         (let [[_ before after] (first(re-seq #"(\d+)\|(\d+)" line))]
+                           [(Integer/parseInt before)
+                            (Integer/parseInt after)]))
 
+      parse-update (fn [line]
+                     (vec (map Integer/parseInt
+                               (str/split
+                                line #","))))
+
+      rules (map parse-page-order rules)
+
+      upd (map parse-update upd)
+
+      check-update (fn [upd rules]
+                     nil)
+
+      ]
+
+  [rules
+   upd]
   )
